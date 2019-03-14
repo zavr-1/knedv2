c9674c88eb8aae922e846dedbb5337aa703f30c6
Anton <anton@adc.sh>
2019-03-14 07:17:37 +0300

    cors proxies

--- WebUpload/index.js
+++ WebUpload/index.js
@@ -2,17 +2,6 @@ require('alamode')()
 const webUpload = require('./web-upload')
 
 module.exports = async (context, req) => {
-  if (req.method == 'OPTIONS') {
-    context.log('OPTIONS REQUEST')
-    return {
-      status: 200,
-      headers: {
-        Allow: 'OPTIONS, POST',
-        'Access-Control-Allow-Origin': req.headers.origin,
-        'Access-Control-Allow-Methods': 'POST, OPTIONS',
-      },
-    }
-  }
   let body, status = 200
   try {
     body = await webUpload(context, req)
@@ -25,8 +14,7 @@ module.exports = async (context, req) => {
     body: JSON.stringify(body, null, 2),
     headers: {
       'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': context.req.headers.origin,
-      'Access-Control-Allow-Methods': 'POST, OPTIONS',
+      'Access-Control-Allow-Origin': req.headers.origin,
     },
   }
 }
